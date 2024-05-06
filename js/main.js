6b09244304d96b3450e9ce634d107bdc513b59d7
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-06 19:42:16 +0200

    [UPD] while loop for inserting shopping list items

--- js/main.js
+++ js/main.js
@@ -10,4 +10,16 @@ const listaDellaSpesa = [];
 
 // richiesta di input tramite propt all'utente dell'inserimento degli elementi della lista della spesa 
 
-let thingsToBuy = prompt('Inserisci un elemento da aggiungere alla tua lista della spesa o scrivi "fine" per terminare')
+let thingsToBuy = prompt('Inserisci un elemento da aggiungere alla tua lista della spesa o scrivi "fine" per terminare');
+
+// ciclo while per l'inserimento degli elementi della lista della spesa
+
+while(thingsToBuy.toLocaleLowerCase() !== "fine"){
+    // push degli elementi della lista della spesa all'interno dell'array vuoto
+    
+    listaDellaSpesa.push(thingsToBuy);
+
+    // chiediamo nuovamente alll'utente di inserire un elemento
+
+    thingsToBuy = prompt('Inserisci un elemento da aggiungere alla tua lista della spesa o scrivi "fine" per terminare');
+}
